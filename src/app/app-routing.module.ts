c9a0c38216b757fda5a790cdf8b2947a6891812a
DanPirs <danilopiresilva122@gmail.com>
2021-10-07 16:19:13 -0300

    implementação das rotas de entrar e cadastrar

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -1,7 +1,15 @@
-import { NgModule } from '@angular/core';
+import { CadastrarComponent } from './cadastrar/cadastrar.component';
+import { NgModule, Component } from '@angular/core';
 import { RouterModule, Routes } from '@angular/router';
+import { EntrarComponent } from './entrar/entrar.component';
 
-const routes: Routes = [];
+const routes: Routes = [
+
+  {path: '', redirectTo: 'entrar', pathMatch: 'full'},
+
+  {path: 'entrar', component:EntrarComponent},
+  {path: 'cadastrar', component:CadastrarComponent}
+];
 
 @NgModule({
   imports: [RouterModule.forRoot(routes)],
---
+++ src/app/model/Tema.ts
@@ -0,0 +1,7 @@
+import { Postagem } from './Postagem';
+
+export class tema{
+  public id: number
+  public descicao: string
+  public postagem: Postagem[]
+}
